2e14d304230f208fe55661184a306873e220cb62
G <0020992@gmail.com>
2023-09-11 23:43:53 +0300

    дз 13.1 задание 1
    1 создан класс items
    2 написаны тесты
    3 мейн работает

--- src/item.py
+++ src/item.py
@@ -1,30 +1,61 @@
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
-    pay_rate = 1.0
+    # Статический список для хранения всех созданных экземпляров класса
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
+    # Коэффициент оплаты
+    pay_rate = 1.0
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+    def __init__(self, name, price, quantity):
+        # Инициализация атрибутов объекта
+        self.name = name  # Название товара
+        self.price = price  # Цена за единицу товара
+        self.quantity = quantity  # Количество товара в магазине
 
-    def calculate_total_price(self) -> float:
+        # Добавляем текущий экземпляр в статический список instances
+        self.__class__.all.append(self)
+
+
+    def calculate_total_price(self):
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
-        :return: Общая стоимость товара.
+        :param name: Название товара для подсчета
+        :return: Общая стоимость товара
         """
-        pass
 
-    def apply_discount(self) -> None:
+        return self.price * self.quantity
+
+    def apply_discount(self):
         """
         Применяет установленную скидку для конкретного товара.
+
+        :param discount: Уровень скидки (например, 0.15 для 15% скидки)
         """
-        pass
+        return self.price * self.pay_rate
+
+    def total_cost(self):
+        """
+        Рассчитывает общую стоимость товара с учетом скидки.
+
+        :return: Общая стоимость товара
+        """
+        if self.quantity < 0:
+            raise ValueError("Количество товара не может быть отрицательным")
+
+        return self.price * self.quantity
+
+    @classmethod
+    def set_pay_rate(cls, rate):
+        """
+        Устанавливает коэффициент оплаты для всех товаров.
+
+        :param rate: Новый коэффициент оплаты
+        """
+        cls.pay_rate = rate
+
+    @classmethod
+    def reset_pay_rate(cls):
+        """
+        Сбрасывает коэффициент оплаты в 1.0 (без скидки).
+        """
+        cls.pay_rate = 1.0
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,64 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+def test_create_item():
+    item = Item("Example Item", 10.0, 5)
+    assert item.name == "Example Item"
+    assert item.price == 10.0
+    assert item.quantity == 5
+
+
+def test_calculate_total_price():
+    item = Item("Item 1", 5.0, 10)  # Include the discount when creating items
+    Item("Item 2", 8.0, 7)
+    Item("Item 1", 5.0, 3)
+
+    price = item.calculate_total_price()
+    assert price == 50.0  # Corrected expected value
+
+
+def test_apply_discount():
+
+    item = Item("Discounted Item", 20.0, 3)
+    Item.pay_rate = 0.8
+    assert item.apply_discount() == 16.0  # Check if the discount is correctly applied
+
+
+def test_total_cost():
+    item = Item("Costly Item", 15.0, 6)
+    cost = item.total_cost()
+    assert cost == 90.0  # Corrected expected value
+
+
+def test_zero_discount():
+    item = Item("Zero Discount Item", 10.0, 5)
+    cost = item.total_cost()
+    assert cost == 50.0  # Check when discount is zero
+
+
+def test_negative_quantity():
+
+    with pytest.raises(ValueError):
+        item = Item("Negative Discount Item", 10.0, -5)  # Check when discount is negative
+        item.total_cost()
+
+
+def test_quantity_zero():
+    item = Item("Zero Quantity Item", 10.0, 0)
+    cost = item.total_cost()
+    assert cost == 0.0  # Check when quantity is zero
+
+
+def test_negative_discount():
+    item = Item("Negative Discount Item", 10.0, 5)
+    cost = item.total_cost()
+    assert cost == 50.0     # Check when discount is negative
+
+def test_pay_rate():
+    item = Item("Negative Discount Item", 10.0, 5)
+    item.set_pay_rate(1.2)
+    assert item.pay_rate == 1.2
+    item.reset_pay_rate()
+    assert item.pay_rate == 1.0
